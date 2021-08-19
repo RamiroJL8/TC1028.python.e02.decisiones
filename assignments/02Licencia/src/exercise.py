431d0af697322f128907aa20a284c7abd589a9fb
RamiroJL8 <89024940+RamiroJL8@users.noreply.github.com>
2021-08-19 08:48:49 -0500

    Update exercise.py

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,10 +1,16 @@
-
-def main():
-    edad = int(input("Ingresa tu edad: "))
-    # Escribe el código adecuado para completar el programa
-    # Para pedir el dato de la idetificación oficial emplea este mensaje:
-    # "¿Tienes identificación oficial? (s/n): "
-
-
-if __name__ == '__main__':
-    main()
+#ramiro Juárez 
+print("--------------------------------------------------------------------------")
+print ()
+edad = int(input("Ingresa tu edad: "))
+if edad <= 0:
+    print("respuesta incorrecta")
+elif edad < 18:
+        print("Usted no puede tramitar su licencia ya que es menor de edad")
+else:
+         Id = input("¿Tienes identificación oficial? ")
+         if Id == "si":
+             print("Usted es candidato a tramitar su licencia ")
+         else:
+            print ("Usted no puede tramitar su licencia sin su identificación oficial")
+print ()
+print("--------------------------------------------------------------------------")
